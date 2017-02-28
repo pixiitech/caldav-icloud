8c52c08fa310a01a3e1b6ccd59fa44af45146645
Gregory Hedrick <gregory@pixiitech.net>
2017-02-27 23:07:38 -0500

    Added recurrence options (rrule, exdate, rdate)

--- lib/caldav-icloud/client.rb
+++ lib/caldav-icloud/client.rb
@@ -17,7 +17,7 @@ module CalDAViCloud
         @proxy_host = proxy_uri.host
         @proxy_port = proxy_uri.port.to_i
       end
-      
+
       uri = URI(data[:uri])
       @host     = uri.host
       @port     = uri.port.to_i
@@ -25,16 +25,16 @@ module CalDAViCloud
       @user     = data[:user]
       @password = data[:password]
       @ssl      = uri.scheme == 'https'
-      
+
       unless data[:authtype].nil?
         @authtype = data[:authtype]
         if @authtype == 'digest'
-        
+
           @digest_auth = Net::HTTP::DigestAuth.new
           @duri = URI.parse data[:uri]
           @duri.user = @user
           @duri.password = @password
-          
+
         elsif @authtype == 'basic'
         #Don't Raise or do anything else
       else
@@ -63,29 +63,29 @@ module CalDAViCloud
       events = []
       res = nil
       __create_http.start {|http|
-        
+
         req = Net::HTTP::Report.new(@url, initheader = {'Content-Type'=>'application/xml'} )
-        
+
     if not @authtype == 'digest'
       req.basic_auth @user, @password
     else
       req.add_field 'Authorization', digestauth('REPORT')
     end
         if data[:start].is_a? Integer
-          req.body = CalDAViCloud::Request::ReportVEVENT.new(Time.at(data[:start]).utc.strftime("%Y%m%dT%H%M%S"), 
+          req.body = CalDAViCloud::Request::ReportVEVENT.new(Time.at(data[:start]).utc.strftime("%Y%m%dT%H%M%S"),
                                                         Time.at(data[:end]).utc.strftime("%Y%m%dT%H%M%S") ).to_xml
         else
-          req.body = CalDAViCloud::Request::ReportVEVENT.new(Time.parse(data[:start]).utc.strftime("%Y%m%dT%H%M%S"), 
+          req.body = CalDAViCloud::Request::ReportVEVENT.new(Time.parse(data[:start]).utc.strftime("%Y%m%dT%H%M%S"),
                                                         Time.parse(data[:end]).utc.strftime("%Y%m%dT%H%M%S") ).to_xml
         end
         res = http.request(req)
-      } 
+      }
         errorhandling res
         result = ""
         #puts res.body
         xml = REXML::Document.new(res.body)
         REXML::XPath.each( xml, '//c:calendar-data/', {"c"=>"urn:ietf:params:xml:ns:caldav"} ){|c| result << c.text}
-        r = Icalendar.parse(result)      
+        r = Icalendar.parse(result)
         unless r.empty?
           r.each do |calendar|
             calendar.events.each do |event|
@@ -101,14 +101,14 @@ module CalDAViCloud
     def find_event uuid
       res = nil
       __create_http.start {|http|
-        req = Net::HTTP::Get.new("#{@url}/#{uuid}.ics")        
+        req = Net::HTTP::Get.new("#{@url}/#{uuid}.ics")
         if not @authtype == 'digest'
           req.basic_auth @user, @password
         else
           req.add_field 'Authorization', digestauth('GET')
         end
         res = http.request( req )
-      }  
+      }
       errorhandling res
       begin
         r = Icalendar.parse(res.body)
@@ -116,10 +116,10 @@ module CalDAViCloud
       rescue
         return false
       else
-        r.first.events.first 
+        r.first.events.first
       end
 
-      
+
     end
 
     def delete_event uuid
@@ -144,24 +144,25 @@ module CalDAViCloud
 
     def create_event event
       c = Icalendar::Calendar.new
-      # c.events = []
       uuid = UUID.new.generate
       raise DuplicateError if entry_with_uuid_exists?(uuid)
       c.event do |e|
-        e.uid = uuid 
+        e.uid = uuid
         e.dtstart = DateTime.parse(event[:start])
         e.dtend = DateTime.parse(event[:end])
-        e.categories = event[:categories]# Array
-        # e.contacts = event[:contacts] # Array
-        # e.attendees = event[:attendees]# Array
+        e.categories = event[:categories]
+        e.contact = event[:contacts]
+        e.attendee = event[:attendees]
         e.duration = event[:duration]
         e.summary = event[:title]
         e.description = event[:description]
-        # e.klass = event[:accessibility] #PUBLIC, PRIVATE, CONFIDENTIAL
         e.location = event[:location]
-        # e.geo_location = event[:geo_location]
+        e.geo = event[:geo_location]
         e.status = event[:status]
         e.url = event[:url]
+        e.rrule = event[:rrule]
+        e.exdate = event[:exdate]
+        e.rdate = event[:rdate]
         if (event[:reminder_before] && event[:reminder_before] != "")
           e.alarm do |a|
             a.action  = "DISPLAY" # This line isn't necessary, it's the default
@@ -201,7 +202,7 @@ module CalDAViCloud
     end
 
     def add_alarm tevent, altCal="Calendar"
-    
+
     end
 
     def find_todo uuid
@@ -214,22 +215,18 @@ module CalDAViCloud
           req.add_field 'Authorization', digestauth('GET')
         end
         res = http.request( req )
-      }  
+      }
       errorhandling res
       r = Icalendar.parse(res.body)
       r.first.todos.first
     end
-
-
-
-
 
     def create_todo todo
       c = Calendar.new
       uuid = UUID.new.generate
       raise DuplicateError if entry_with_uuid_exists?(uuid)
       c.todo do
-        uid           uuid 
+        uid           uuid
         start         DateTime.parse(todo[:start])
         duration      todo[:duration]
         summary       todo[:title]
@@ -275,32 +272,32 @@ module CalDAViCloud
         req.body = CalDAViCloud::Request::ReportVTODO.new.to_xml
         res = http.request( req )
       }
-      errorhandling res 
+      errorhandling res
       format.parse_todo( res.body )
     end
 
     private
-    
+
     def digestauth method
-    
+
       h = Net::HTTP.new @duri.host, @duri.port
       if @ssl
         h.use_ssl = @ssl
         h.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
       req = Net::HTTP::Get.new @duri.request_uri
-      
+
       res = h.request req
       # res is a 401 response with a WWW-Authenticate header
-      
+
       auth = @digest_auth.auth_header @duri, res['www-authenticate'], method
-      
+
       return auth
     end
-    
+
     def entry_with_uuid_exists? uuid
       res = nil
-      
+
       __create_http.start {|http|
         req = Net::HTTP::Get.new("#{@url}/#{uuid}.ics")
         if not @authtype == 'digest'
@@ -308,9 +305,9 @@ module CalDAViCloud
         else
           req.add_field 'Authorization', digestauth('GET')
         end
-        
+
         res = http.request( req )
-        
+
       }
       begin
         errorhandling res
@@ -321,10 +318,10 @@ module CalDAViCloud
         return true
       end
     end
-    def  errorhandling response   
+    def  errorhandling response
       raise NotExistError if response.code.to_i == 404
       raise AuthenticationError if response.code.to_i == 401
-      raise NotExistError if response.code.to_i == 410 
+      raise NotExistError if response.code.to_i == 410
       raise APIError if response.code.to_i >= 500
     end
   end
